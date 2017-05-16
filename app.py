a38e06efd4a837444abd5a6195a6d2950b5d69eb
Andrew Harris <blakebharris@gmail.com>
2017-05-16 13:10:16 -0500

    another route and debug mode

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.config["DEBUG"] = True
 
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!??????'
 
 
 @app.route("/test/<search_query>")
@@ -30,6 +30,14 @@ def float_type(value):
 def path_type(value):
     print(value)
     return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == 'blake':
+        return "Hello {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 if __name__ == '__main__':
     app.run()
